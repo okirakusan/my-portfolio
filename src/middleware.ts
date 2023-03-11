92d9f815148fe07690a738d4eab0cb37ea7385cc
okirakusan <oimbt0049@gmail.com>
2023-03-11 20:53:50 +0900

    ♻️ BASIC認証の為のリファクタリング(10''')

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,24 +7,27 @@ export const config = {
   matcher: ["/:path*"],
 };
 
-export function middleware(req: NextRequest) {
-  const url = req.nextUrl;
-  if (!url.pathname.startsWith("/")) return;
+export const middleware = (req: NextRequest) => {
+  if (!req.nextUrl.pathname.startsWith("/")) {
+    const basicAuth = req.headers.get("authorization");
 
-  const basicAuth = req.headers.get("authorization");
+    if (basicAuth) {
+      const auth = basicAuth.split(" ")[1];
+      const [user, pwd] = atob(auth).split(":");
 
-  if (basicAuth) {
-    const auth = basicAuth.split(" ")[1];
-    const [user, pwd] = atob(auth).split(":");
-
-    if (
-      user === process.env.BASIC_AUTH_USER &&
-      pwd === process.env.BASIC_AUTH_PASSWORD
-    ) {
-      return NextResponse.next();
+      if (
+        user === process.env.BASIC_AUTH_USER &&
+        pwd === process.env.BASIC_AUTH_PASSWORD
+      ) {
+        return NextResponse.next();
+      }
     }
-  }
-  url.pathname = "/api/auth";
 
-  return NextResponse.rewrite(url);
-}
+    const url = req.nextUrl;
+    url.pathname = "/api/auth";
+
+    return NextResponse.rewrite(url);
+  }
+
+  return NextResponse.next();
+};
