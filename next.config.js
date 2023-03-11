84832c4fec815817e239e79ed97b171bf8da34d2
okirakusan <oimbt0049@gmail.com>
2023-03-11 17:59:12 +0900

    ♻️ BASIC認証の為のリファクタリング(4)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   env: {
     BASIC_AUTH_USER: process.env.BASIC_AUTH_USER,
     BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD,
