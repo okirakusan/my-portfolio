8947b34572786992a40e357f089479c9e9f8c409
okirakusan <oimbt0049@gmail.com>
2023-03-11 21:19:57 +0900

    ♻️ BASIC認証の為のリファクタリング(10'''''')

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,11 @@ import { NextRequest, NextResponse } from "next/server";
 matcher : middleware.jsを適用する（呼び出す）パスを指定する
 */
 // export const config = {
-//   matcher: ["/index/:path*"],
+//   matcher: ["/:path*"],
 // };
 
 export const middleware = (req: NextRequest) => {
-  if (!req.nextUrl.pathname.startsWith("/auth")) {
+  if (req.nextUrl.pathname.startsWith("/auth")) {
     const basicAuth = req.headers.get("authorization");
 
     if (basicAuth) {
