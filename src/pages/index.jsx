eba02deff2afd44e9ea39b85896c7e9beb784a22
okirakusan <oimbt0049@gmail.com>
2023-03-31 00:23:21 +0900

    ✨  メニュー単体での計算機能追加♻️ publicフォルダを適正に配置修正

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,17 +3,17 @@ import {
   AiFillLinkedin,
   AiFillYoutube,
 } from "react-icons/ai";
-import deved from "public/dev-ed-wave.png";
-import code from "public/code.png";
-import design from "public/design.png";
-import consulting from "public/consulting.png";
+import deved from "/public/dev-ed-wave.png";
+import code from "/public/code.png";
+import design from "/public/design.png";
+import consulting from "/public/consulting.png";
 import Image from "next/image";
-import web1 from "public/web1.png";
-import web2 from "public/web2.png";
-import web3 from "public/web3.png";
-import web4 from "public/web4.png";
-import web5 from "public/web5.png";
-import web6 from "public/web6.png";
+import web1 from "/public/web1.png";
+import web2 from "/public/web2.png";
+import web3 from "/public/web3.png";
+import web4 from "/public/web4.png";
+import web5 from "/public/web5.png";
+import web6 from "/public/web6.png";
 
 export default function Home() {
   return (
--- src/pages/priceRangeMenuSelector.jsx
+++ src/pages/priceRangeMenuSelector.jsx
@@ -32,6 +32,21 @@ const MenuApp = () => {
     setOrderedItems([...orderedItems, item]);
   };
 
+  const addOneMore = (item) => {
+    setTotal(total + item.price);
+    setOrderedItems([...orderedItems, item]);
+  };
+
+  const clearOne = (item) => {
+    const newItems = [...orderedItems];
+    const index = newItems.findIndex((i) => i.id === item.id);
+    if (index !== -1) {
+      const removedItem = newItems.splice(index, 1)[0];
+      setTotal(total - removedItem.price);
+      setOrderedItems(newItems);
+    }
+  };
+
   const clearOrder = () => {
     setTotal(0);
     setOrderedItems([]);
@@ -61,9 +76,9 @@ const MenuApp = () => {
 
   return (
     <>
-      <section className=" pb-40 dark:text-white h-auto">
-        <h1 className="text-3xl font-bold mb-8">
-          Menuをランダムに選んでくれるアプリ
+      <section className=" pb-40 dark:text-white h-2/4">
+        <h1 className="text-3xl font-bold mb-8 text-center">
+          メニューをランダムに選んでくれるアプリ
         </h1>
         <button
           onClick={clearMenu}
@@ -71,7 +86,7 @@ const MenuApp = () => {
         >
           メニュー再取得
         </button>
-        <form className="mb-8">
+        <form className="mb-2">
           <label htmlFor="price" className="mr-4">
             設定金額:
           </label>
@@ -85,7 +100,7 @@ const MenuApp = () => {
           />
           <p>※1500円で全メニューからランダムに取得できます</p>
         </form>
-        <ul className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <ul className="grid gap-4 grid-cols-2 lg:grid-cols-3">
           {selectedMenu.map((item) => (
             <li key={item.id} className="border border-gray-400 p-4">
               <div className="font-bold mb-2">{item.name}</div>
@@ -95,6 +110,7 @@ const MenuApp = () => {
                 width={200}
                 height={100}
                 layout="responsive"
+                objectFit="contain"
                 className="mb-2"
               />
               <div className="text-lg mb-2">{item.price}円</div>
@@ -107,24 +123,44 @@ const MenuApp = () => {
             </li>
           ))}
         </ul>
-        <h2 className="text-2xl font-bold my-8">注文済み</h2>
-        <ul className="grid gap-2 md:grid-cols-2 lg:grid-cols-3 ">
+        <h2 className="text-2xl font-bold mt-6 mb-2">注文済み</h2>
+        <ul className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
           {orderedItems.map((item, index) => {
             const count = orderedItems.filter((i) => i.id === item.id).length;
             if (index === orderedItems.findIndex((i) => i.id === item.id)) {
               return (
-                <li key={item.id} className="border border-gray-400 p-4">
-                  <div className="font-bold mb-2">{item.name}</div>
+                <li
+                  key={item.id}
+                  className="border border-gray-400 p-4 bg-yellow-300 text-black"
+                >
+                  <div className="flex justify-between items-center">
+                    <div className="font-bold mb-2">{item.name}</div>
+                    <div className="text-lg mb-2">
+                      {item.price}円 × {count}点
+                    </div>
+                  </div>
                   <Image
                     src={`${item.image}`}
                     alt={`${item.name}`}
                     width={200}
                     height={100}
                     layout="responsive"
+                    objectFit="contain"
                     className="mb-2"
                   />
-                  <div className="text-lg mb-2">
-                    {item.price}円 × {count}点
+                  <div className="flex justify-between items-center">
+                    <button
+                      onClick={() => addOneMore(item)}
+                      className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600"
+                    >
+                      +1
+                    </button>
+                    <button
+                      onClick={() => clearOne(item)}
+                      className="bg-red-500 text-white px-4 py-2 rounded-full hover:bg-red-600"
+                    >
+                      -1
+                    </button>
                   </div>
                 </li>
               );
@@ -132,13 +168,13 @@ const MenuApp = () => {
               return null;
             }
           })}
-        </ul>
+        </ul>{" "}
         <h2 className="text-2xl font-bold my-8">合計: {total}円</h2>
         <button
           onClick={clearOrder}
           className="bg-red-500 text-white px-4 py-2 rounded-full hover:bg-red-600"
         >
-          注文クリア
+          全注文クリア
         </button>
       </section>
     </>
