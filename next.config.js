0d1594b973d0f76eb72414d8d4ed822eb31b22f1
okirakusan <oimbt0049@gmail.com>
2023-04-09 18:55:51 +0900

    ✨ openaiApiに関するenvファイル設定の追加

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
   env: {
     BASIC_AUTH_USER: process.env.BASIC_AUTH_USER,
     BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD,
+    organization: process.env.NEXT_PUBLIC_OPENAI_ORG,
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
   },
   images: {
     domains: ["cdn2.thecatapi.com"],
