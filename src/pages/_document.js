1dd978344ab38de48065970f021780a79d7f0280
okirakusan <oimbt0049@gmail.com>
2023-04-14 22:00:17 +0900

    ♻️ メタ設定修正

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ja">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
