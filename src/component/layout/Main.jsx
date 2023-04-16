5234a80b70d9b7eb75b0881380bd220b8295b5b4
okirakusan <oimbt0049@gmail.com>
2023-04-16 22:15:05 +0900

    ♻️スタイル調整

--- src/component/layout/Main.jsx
+++ src/component/layout/Main.jsx
@@ -1,6 +1,6 @@
 export const Main = (props) => {
   return (
-    <main className=" bg-white dark:bg-gray-900 px-10 md:px-20 lg:px-30 min-h-screen sm:pt-28 lg:pt-36 overflow-auto">
+    <main className=" bg-white dark:bg-gray-900 px-10 md:px-20 lg:px-30 min-h-screen sm:pt-28 lg:pt-36 pb-20 sm:pb-36 overflow-auto">
       {props.children}
     </main>
   );
